9b602308438b104f658e47c8802b33dfedd487ff
Ramiro Aquistapace <ramiaquista70@gmail.com>
2022-01-31 16:41:08 -0300

    Change the type of column 'measurement_unit' from integer to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_170608) do
+ActiveRecord::Schema.define(version: 2022_01_31_193814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
-    t.integer "measurement_unit"
+    t.string "measurement_unit"
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
