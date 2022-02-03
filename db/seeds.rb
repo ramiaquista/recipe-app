a3cb89662b0306ecf0d85cf15b4670e0e5aeb45d
Luis Fernando <lfmnovaes@gmail.com>
2022-02-03 13:32:00 -0300

    Style recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,15 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 user = User.create(name: "John", email: "john@mail.com", password: "qwe123")
-apple = user.foods.create(name: "apple", measurement_unit: "unit", price: 4)
-wheat = user.foods.create(name: "wheat", measurement_unit: "gram", price: 0.5)
-sugar = user.foods.create(name: "sugar", measurement_unit: "gram", price: 0.2)
+apple = user.foods.create(name: "Apple", measurement_unit: "unit", price: 4)
+wheat = user.foods.create(name: "Wheat", measurement_unit: "gram", price: 0.5)
+sugar = user.foods.create(name: "Sugar", measurement_unit: "gram", price: 0.2)
 user.save
-recipe = Recipe.create(name: "apple pie", preparation_time: 30, cooking_time: 40, description: "lorem ipsum", public: true, user_id: user.id)
+recipe = Recipe.create(name: "Apple Pie", preparation_time: 30, cooking_time: 40, description: "lorem ipsum", public: true, user_id: user.id)
 recipe.recipe_foods.create(quantity: 6, food_id: apple.id)
 recipe.recipe_foods.create(quantity: 200, food_id: wheat.id)
 recipe.save
-recipe2 = Recipe.create(name: "apple candy", preparation_time: 10, cooking_time: 10, description: "candy", public: false, user_id: user.id)
+recipe2 = Recipe.create(name: "Apple Candy", preparation_time: 10, cooking_time: 10, description: "candy", public: false, user_id: user.id)
 recipe2.recipe_foods.create(quantity: 1, food_id: apple.id)
 recipe2.recipe_foods.create(quantity: 100, food_id: sugar.id)
 recipe2.save
